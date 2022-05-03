557c0a36b1bc550bd24b4cfea568144e2594c301
avazbekmurataliyev <97029745+avazbekmurataliyev@users.noreply.github.com>
2022-05-03 22:57:45 +0500

    Delete springboot directory

--- springboot/repository/PassportRepository.java
+++
@@ -1,9 +0,0 @@
-package uz.avazbek.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.avazbek.springboot.model.Passport;
-
-public interface PassportRepository extends JpaRepository<Passport,Long> {
-
-
-}
